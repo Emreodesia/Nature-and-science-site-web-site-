9ff9d9613c8b58169eb7e046b4dc77dc43ba96a6
Emre <emre.bahar35@outlook.com>
2024-01-09 20:40:55 +0300

    jQuery

---
+++ One day One Stroy/File/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $("body").append("<p>Hello World</p>");
+});
+
+
+
+$(document).ready(function(){
+    $("img").click(function(){
+        $(this).fadeOut();
+    });
+});

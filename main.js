0ed6f63db10aa3f85e566a729904a4e45cc58779
Emre <emre.bahar35@outlook.com>
2024-02-09 15:12:25 +0300

    İlk commit

---
+++ main.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $("body").append("<p>Hello World :D</p>");
+});
+
+
+
+$(document).ready(function(){
+    $("Nav1").click(function(){
+        $(this).fadeOut();
+    });
+});
